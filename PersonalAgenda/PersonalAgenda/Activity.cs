c3131aa4e64912a1fde94ef283a0110ad83eba6f
George <george.balot7@yahoo.com>
2020-04-23 19:40:31 +0300

    Initial commit

---
+++ PersonalAgenda/PersonalAgenda/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaPersonala
+{
+    class Activity
+    {
+        public string Name;
+        public string Description;
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public List<Person> PeopleInvolved;
+    }
+}
---
+++ PersonalAgenda/PersonalAgenda/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaPersonala
+{
+    class Person
+    {
+        public string LastName;
+        public string FirstName;
+        public DateTime BirthDate;
+        public string EmailAddress;
+        public Agenda Agenda;
+    }
+}
